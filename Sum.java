05e579b9a170cd4162655d088d3e92e84ece590a
sai9133 <gsaigsai19919@gmail.com>
2023-04-21 19:44:49 +0530

    Sum program

---
+++ Sum.java
@@ -0,0 +1,17 @@
+class Sum
+ {
+	public static int addSum(int a,int b)
+	{
+		return (a+b);
+	}
+                       public static int substraction(int c,int d)
+	{
+	      return (c-d);
+	}
+
+	public static void main (String [] args)
+                        {
+	      System.out.println("the sum of two numbers"+addSum(10,20));
+   	       System.out.println("the substraction of two numbers"+ substraction(10,20));
+	}
+}
